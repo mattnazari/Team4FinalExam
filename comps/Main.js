d519880a2c12358ca88249abfd38e25b27e06290
matthewnazari <matthew.nazari8@gmail.com>
2019-12-10 13:20:37 -0800

    initial setup

---
+++ comps/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Main = () =>{
+  return(
+    <View>
+        {/* Add your component here */}
+    </View>
+  )
+}
+
+export default Main;
