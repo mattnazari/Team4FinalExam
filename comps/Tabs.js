78a16ffbe3720af8d8dff0d8408e0e5635ad8a48
NicolasAsis <nico.asis15@gmail.com>
2019-12-11 12:50:02 -0800

    Did most of menu

--- comps/Tabs.js
+++ comps/Tabs.js
@@ -8,24 +8,26 @@ import {
   Image
 } from "react-native";
 
-const Tabs = () => {
+const Tabs = ({title,onPress,defColor,setDefColor,defColor2,setDefColor2}) => {
   const [tabWidth] = useState(new Animated.Value(0));
   const [tabHeight] = useState(new Animated.Value(0));
   const [tabColor] = useState(new Animated.Value(0));
 
+  // const [tabColor2,setTabcolor2] = useState(defColor);
+
   var myTabWidth = tabWidth.interpolate({
     inputRange: [0, 1],
-    outputRange: ["0%", "25%"]
+    outputRange: ["0%", "98%"]
   });
 
   var myTabHeight = tabHeight.interpolate({
     inputRange: [0, 1],
-    outputRange: ["0%", "8%"]
+    outputRange: ["0%", "10%"]
   });
 
   var myTabColor = tabColor.interpolate({
     inputRange: [0, 1],
-    outputRange: ["#000000", "#57af9b"]
+    outputRange: ["#000000", defColor]
   });
 
   useEffect(() => {
@@ -65,21 +67,30 @@ const Tabs = () => {
           height: myTabHeight,
           backgroundColor: myTabColor,
           alignItems: "center",
-          borderTopLeftRadius: 5,
-          borderTopRightRadius: 5
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10
         }}
       >
-        <TouchableOpacity style={{flex:1, justifyContent:'center', alignItems:'center', width:200, height:200}}>
+        <TouchableOpacity 
+          style={{flex:1, justifyContent:'center', alignItems:'center', width:200, height:200}}
+          onPress={onPress}
+        >
   
           <Image
             style={{ width: "20%", height: "20%", padding:20 }}
             source={require("../tabImg.png")}
           />
-          <Text style={{ color: "white" }}>Default Tab</Text>
+          <Text style={{ color: "white" }}>{title}</Text>
           </TouchableOpacity>
       </Animated.View>
     </View>
   );
+};
+
+Tabs.defaultProps = {
+  title:"Default Title",
+  onPress: () => {},
+  defColor:"#57af9b"
 };
 
 export default Tabs;
