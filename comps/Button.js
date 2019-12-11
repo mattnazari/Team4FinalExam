c1961802424d289c0c0c1b622fb1ff1b58560743
matthewnazari <matthew.nazari8@gmail.com>
2019-12-11 10:31:44 -0800

    Button comp styling

--- comps/Button.js
+++ comps/Button.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, TouchableOpacity, Animated, Text} from 'react-native';
 
 const Button = () =>{
+
   return(
-    <View>
-        {/* Add your component here */}
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <TouchableOpacity style={{flexDirection:"row", alignItems:'center', backgroundColor:'pink', borderRadius:20, padding:10}}>
+      <Image
+      style={{width:50, height:50}}
+      source={{uri:'https://static.thenounproject.com/png/68669-200.png'}}
+      />
+      <Text style={{color:'white'}}>{'Default\nButton'}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
