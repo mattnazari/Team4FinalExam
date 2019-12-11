2210cbd34472cf379bf5ae67c13a5603b7b72fd1
matthewnazari <matthew.nazari8@gmail.com>
2019-12-11 09:37:08 -0800

    initial setup for comps

---
+++ comps/Tabs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Tabs = () =>{
+  return(
+    <View>
+        {/* Add your component here */}
+    </View>
+  )
+}
+
+export default Tabs;
---
+++ comps/TextInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const TextInput = () =>{
+  return(
+    <View>
+        {/* Add your component here */}
+    </View>
+  )
+}
+
+export default TextInput;
