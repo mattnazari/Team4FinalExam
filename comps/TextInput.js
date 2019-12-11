3e5233a048df8e1a4f2f4980e4fd57080dda40f1
heiditang <heiditang128@gmail.com>
2019-12-11 09:53:44 -0800

    created Text Input

--- comps/TextInput.js
+++ comps/TextInput.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, TextInput, StyleSheet, Animated} from 'react-native';
 
 const TextInput = () =>{
   return(
     <View>
-        {/* Add your component here */}
+        <TextInput style={styles.txtinput}></TextInput>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  txtinput:{
+      flex:1,
+      width: '100%',
+      height: '50%',
+      backgroundColor: 'blue',
+  }
+})
+
 
 export default TextInput;
