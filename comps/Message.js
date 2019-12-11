933bd222894c81becfcc42048eed3b97c147d3e4
bin <quadclipse@gmail.com>
2019-12-11 10:10:05 -0800

    Message styling done

--- comps/Message.js
+++ comps/Message.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TouchableOpacity, Animated, Text, TextInput, Image, StyleSheet} from 'react-native';
 
-const Message = () =>{
+function Message (){
+
+
   return(
+
     <View>
-        {/* Add your component here */}
+
+        <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'center', width:'auto', height:100, backgroundColor:'rgb(80,100,120)', borderRadius:10}}>
+
+          <Image 
+            style={{ margin:20,  width:50, height:50, borderRadius:50, borderColor:'green', borderWidth:2}}
+            source={require('./default-placeholder.png')}
+          />
+          <View style={{flexDirection:'row', marginRight:20, width:'auto', alignItems:'center', justifyContent:'center',}} >
+            <Text style={MessageStyle.GuestText}>guest: </Text>
+            <Text style={MessageStyle.GuestMessage}>Default Message</Text>
+          </View>
+        </TouchableOpacity>
+
     </View>
+
   )
 }
+
+var MessageStyle = StyleSheet.create({
+
+  GuestText:{
+    fontSize:15,
+  },
+  GuestMessage:{
+    color:'azure',
+    fontSize:15
+  }
+
+})
 
 export default Message;
